701866417c51fcfdb676d7834bc6d24caf5aaa78
Dev Keshwani <dev.keshwani345@gmail.com>
2023-07-12 23:37:25 +0530

    fix: code updated

--- traffic-light-circuit/src/main.cpp
+++ traffic-light-circuit/src/main.cpp
@@ -3,12 +3,37 @@
 void setup() {
   // put your setup code here, to run once:
   pinMode(12, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(8, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  //RED
   digitalWrite(12, HIGH);
-  delay(1000);
+  digitalWrite(10, LOW);
+  digitalWrite(8, LOW);
+  delay(7000);
+  //YELLOW
   digitalWrite(12, LOW);
-  delay(1000); 
+  digitalWrite(10, HIGH);
+  digitalWrite(8, LOW);
+  delay(4000);
+  //GREEN
+  digitalWrite(12, LOW);
+  digitalWrite(10, LOW);
+  digitalWrite(8, HIGH);
+  delay(4000);
+  digitalWrite(8, LOW);
+  delay(700);
+  digitalWrite(8,HIGH);
+  delay(700);
+  digitalWrite(8, LOW);
+  delay(700);
+  digitalWrite(8,HIGH);
+  //REPEAT
+  delay(1000);
+
+
+
 }
