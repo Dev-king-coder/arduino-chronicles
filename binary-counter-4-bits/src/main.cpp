c9e1752405fc605bb80a357693e148e6760e8506
Dev Keshwani <dev.keshwani345@gmail.com>
2023-07-19 00:16:17 +0530

    code added in binary-counter

--- binary-counter-4-bits/src/main.cpp
+++ binary-counter-4-bits/src/main.cpp
@@ -1,18 +1,27 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int LED_BIT_1 = 6;
+int LED_BIT_2 = 8;
+int LED_BIT_3 = 10;
+int LED_BIT_4 = 12;
+int DELAY_CYCLE = 1000;
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_BIT_1, OUTPUT);
+  pinMode(LED_BIT_2, OUTPUT);
+  pinMode(LED_BIT_3, OUTPUT);
+  pinMode(LED_BIT_4, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  int memory_array[16][4]={{0,0,0,0},{0,0,0,1},{0,0,1,0},{0,0,1,1},{0,1,0,0},{0,1,0,1},{0,1,1,0},{0,1,1,1},{1,0,0,0},{1,0,0,1},{1,0,1,0},{1,0,1,1},{1,1,0,0},{1,1,0,1},{1,1,1,0},{1,1,1,1}};
+  for(int i=0;i<16;i++){
+    digitalWrite(LED_BIT_1, memory_array[i][0]);
+    digitalWrite(LED_BIT_2, memory_array[i][1]);
+    digitalWrite(LED_BIT_3, memory_array[i][2]);
+    digitalWrite(LED_BIT_4, memory_array[i][3]);
+    delay(DELAY_CYCLE);
+  }
 }
