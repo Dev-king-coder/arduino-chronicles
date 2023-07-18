b1ff91884cb83d95f7d6f50d4dacaebc3c1d5b84
Dev Keshwani <dev.keshwani345@gmail.com>
2023-07-18 23:34:29 +0530

    code added to project-'sos-message'

--- sos-message/src/main.cpp
+++ sos-message/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
-
+int PIN_SOS_LED =12;
+int DELAY_DOT = 200;
+int DELAY_DASH = 700;
+int DELAY_BETWEEN = 400; 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(PIN_SOS_LED, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  for(int i=0; i<3; i++){
+    dot();
+  }
+  for(int i=0; i<3; i++){
+    dash();
+  }
+  for(int i=0; i<3; i++){
+    dot();
+  }
+  delay(1000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+void dash(){
+  digitalWrite(PIN_SOS_LED, HIGH);
+  delay(DELAY_DASH);
+  digitalWrite(PIN_SOS_LED, LOW);
+  delay(DELAY_BETWEEN);
+}
+
+void dot(){
+  digitalWrite(PIN_SOS_LED, HIGH);
+  delay(DELAY_DOT);
+  digitalWrite(PIN_SOS_LED, LOW);
+  delay(DELAY_BETWEEN);
 }
