02e43feb577b11c245a113450106e7bf7751d5f4
Dev Keshwani <dev.keshwani345@gmail.com>
2023-07-18 23:46:35 +0530

    fix: patched function undefined error

--- sos-message/src/main.cpp
+++ sos-message/src/main.cpp
@@ -2,25 +2,11 @@
 
 int PIN_SOS_LED =12;
 int DELAY_DOT = 200;
-int DELAY_DASH = 700;
+int DELAY_DASH = 800;
 int DELAY_BETWEEN = 400; 
 void setup() {
   // put your setup code here, to run once:
   pinMode(PIN_SOS_LED, OUTPUT);
-}
-
-void loop() {
-  // put your main code here, to run repeatedly:
-  for(int i=0; i<3; i++){
-    dot();
-  }
-  for(int i=0; i<3; i++){
-    dash();
-  }
-  for(int i=0; i<3; i++){
-    dot();
-  }
-  delay(1000);
 }
 
 void dash(){
@@ -35,4 +21,18 @@ void dot(){
   delay(DELAY_DOT);
   digitalWrite(PIN_SOS_LED, LOW);
   delay(DELAY_BETWEEN);
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  for(int i=0; i<3; i++){
+    dot();
+  }
+  for(int i=0; i<3; i++){
+    dash();
+  }
+  for(int i=0; i<3; i++){
+    dot();
+  }
+  delay(1000);
 }
