c0414b07442d3a25a394f02251c9291d42de37fb
Dev Keshwani <dev.keshwani345@gmail.com>
2023-07-18 21:04:23 +0530

    fix: added variables

--- traffic-light-circuit/src/main.cpp
+++ traffic-light-circuit/src/main.cpp
@@ -1,39 +1,43 @@
 #include <Arduino.h>
 
+// variables
+#define PIN_RED 12
+#define PIN_YELLOW 10
+#define PIN_GREEN 8
+#define DELAY_GO 4000
+#define DELAY_BLINK 700
+
 void setup() {
   // put your setup code here, to run once:
-  pinMode(12, OUTPUT);
-  pinMode(10, OUTPUT);
-  pinMode(8, OUTPUT);
+  pinMode(PIN_RED, OUTPUT);
+  pinMode(PIN_YELLOW, OUTPUT);
+  pinMode(PIN_GREEN, OUTPUT);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
   //RED
-  digitalWrite(12, HIGH);
-  digitalWrite(10, LOW);
-  digitalWrite(8, LOW);
+  digitalWrite(PIN_RED, HIGH);
+  digitalWrite(PIN_YELLOW, LOW);
+  digitalWrite(PIN_GREEN, LOW);
   delay(7000);
   //YELLOW
-  digitalWrite(12, LOW);
-  digitalWrite(10, HIGH);
-  digitalWrite(8, LOW);
+  digitalWrite(PIN_RED, LOW);
+  digitalWrite(PIN_YELLOW, HIGH);
+  digitalWrite(PIN_GREEN, LOW);
   delay(4000);
   //GREEN
-  digitalWrite(12, LOW);
-  digitalWrite(10, LOW);
-  digitalWrite(8, HIGH);
-  delay(4000);
-  digitalWrite(8, LOW);
-  delay(700);
-  digitalWrite(8,HIGH);
-  delay(700);
-  digitalWrite(8, LOW);
-  delay(700);
-  digitalWrite(8,HIGH);
+  digitalWrite(PIN_RED, LOW);
+  digitalWrite(PIN_YELLOW, LOW);
+  digitalWrite(PIN_GREEN, HIGH);
+  delay(DELAY_GO);
+  digitalWrite(PIN_GREEN, LOW);
+  delay(DELAY_BLINK);
+  digitalWrite(PIN_GREEN,HIGH);
+  delay(DELAY_BLINK);
+  digitalWrite(PIN_GREEN, LOW);
+  delay(DELAY_BLINK);
+  digitalWrite(PIN_GREEN,HIGH);
   //REPEAT
   delay(1000);
-
-
-
 }
