de68c494272d1f6d1fbadeb36da4335e913d6e9a
Dev Keshwani <dev.keshwani345@gmail.com>
2023-07-19 23:47:53 +0530

    code added to larsen-scanner

--- larsen-scanner/src/main.cpp
+++ larsen-scanner/src/main.cpp
@@ -1,18 +1,38 @@
 #include <Arduino.h>
 
-// put function declarations here:
-int myFunction(int, int);
+int LED_1 = 6;
+int LED_2 = 8;
+int LED_3 = 10;
+int LED_4 = 12;
+int LED_ARRAY[4]={LED_1,LED_2,LED_3,LED_4};
+int DELAY_CYCLE = 50;
 
-void setup() {
+void setup()
+{
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  pinMode(LED_1, OUTPUT);
+  pinMode(LED_2, OUTPUT);
+  pinMode(LED_3, OUTPUT);
+  pinMode(LED_4, OUTPUT);
 }
 
-void loop() {
+void loop()
+{
   // put your main code here, to run repeatedly:
-}
-
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
+  for(int i=0;i<4;i++){
+    digitalWrite(LED_ARRAY[i], HIGH);
+    delay(50);
+    digitalWrite(LED_ARRAY[i+1], HIGH);
+    delay(50);
+    digitalWrite(LED_ARRAY[i], LOW);
+    delay(50);
+  }
+  for(int i=4;i>0;i--){
+    digitalWrite(LED_ARRAY[i], HIGH);
+    delay(50);
+    digitalWrite(LED_ARRAY[i-1], HIGH);
+    delay(50);
+    digitalWrite(LED_ARRAY[i], LOW);
+    delay(50);
+  }
 }
